f6061b457c141181e71dc3b3fb8f1a95d39e0890
vanhung88 <88levanhung@gmail.com>
2022-04-16 21:47:46 +0700

    test3

--- a.js
+++ a.js
@@ -1,7 +1,7 @@
 ////
 const test=() => {
     console.log("TEST rebase")
-    console.log("TEST rebase test 2")
+    console.log("TEST rebase test 333")
 }
 const test2=() => {
     console.log("TEST2")
